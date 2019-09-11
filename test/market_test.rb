97459d0d42afed8b745c7577f4fbda19bdf349c6
Graham Thompson <grwthomps@gmail.com>
2019-09-11 14:12:24 -0600

    Add tests for sorted_item_list and total_inventory

--- test/market_test.rb
+++ test/market_test.rb
@@ -15,6 +15,9 @@ class MarketTest < Minitest::Test
     @vendor_2.stock("Peach-Raspberry Nice Cream", 25)
     @vendor_3 = Vendor.new("Palisade Peach Shack")
     @vendor_3.stock("Peaches", 65)
+    @market.add_vendor(@vendor_1)
+    @market.add_vendor(@vendor_2)
+    @market.add_vendor(@vendor_3)
   end
 
   def test_it_exists
@@ -23,32 +26,27 @@ class MarketTest < Minitest::Test
 
   def test_initialization
     assert_equal "South Pearl Street Farmers Market", @market.name
-    assert_equal [], @market.vendors
   end
 
   def test_adding_vendors
-    @market.add_vendor(@vendor_1)
-    @market.add_vendor(@vendor_2)
-    @market.add_vendor(@vendor_3)
-
     assert_equal [@vendor_1, @vendor_2, @vendor_3], @market.vendors
   end
 
   def test_accessing_vendor_names
-    @market.add_vendor(@vendor_1)
-    @market.add_vendor(@vendor_2)
-    @market.add_vendor(@vendor_3)
-
     assert_equal ["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"], @market.vendor_names
   end
 
   def test_vendors_that_sell
-    @market.add_vendor(@vendor_1)
-    @market.add_vendor(@vendor_2)
-    @market.add_vendor(@vendor_3)
-
     assert_equal [@vendor_1, @vendor_3], @market.vendors_that_sell("Peaches")
     assert_equal [@vendor_2], @market.vendors_that_sell("Banana Nice Cream")
+  end
+
+  def test_sorted_item_list
+    assert_equal ["Banana Nice Cream", "Peach-Raspberry Nice Cream", "Peaches", "Tomatoes"], @market.sorted_item_list
+  end
+
+  def test_total_inventory
+    assert_equal ({"Peaches"=>100, "Tomatoes"=>7, "Banana Nice Cream"=>50, "Peach-Raspberry Nice Cream"=>25}), @market.total_inventory
   end
 
 end
