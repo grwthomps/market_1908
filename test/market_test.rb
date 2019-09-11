fe372628592beef83b9aba0618b18313c4bae297
Graham Thompson <grwthomps@gmail.com>
2019-09-11 13:19:07 -0600

    Create market class file and market test file

---
+++ test/market_test.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/market'
+require './lib/vendor'
+
+class MarketTest < Minitest::Test
+
+  def setup
+
+  end
+
+  def test_it_exists
+
+  end
+
+  def test_initialization
+
+  end
+
+end
