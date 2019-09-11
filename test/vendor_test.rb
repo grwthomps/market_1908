5ce710eb82384cae1308ec40b24b31d185e0b312
Graham Thompson <grwthomps@gmail.com>
2019-09-11 13:15:59 -0600

    Add tests for initialize, check_stock, stock, and inventory

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -4,4 +4,34 @@ require './lib/vendor'
 
 class VendorTest < Minitest::Test
 
+  def setup
+    @vendor = Vendor.new("Rocky Mountain Fresh")
+  end
+
+  def test_it_exists
+    assert_instance_of Vendor, @vendor
+  end
+
+  def test_initialization
+    assert_equal "Rocky Mountain Fresh", @vendor.name
+    assert_equal ({}), @vendor.inventory
+  end
+
+  def test_adding_to_its_stock
+    assert_equal 0, @vendor.check_stock("Peaches")
+    @vendor.stock("Peaches", 30)
+    assert_equal 30, @vendor.check_stock("Peaches")
+    @vendor.stock("Peaches", 25)
+    assert_equal 55, @vendor.check_stock("Peaches")
+  end
+
+  def test_its_inventory
+    
+    @vendor.stock("Peaches", 30)
+    @vendor.stock("Peaches", 25)
+    @vendor.stock("Tomatoes", 12)
+
+    assert_equal ({"Peaches"=>55, "Tomatoes"=>12}), @vendor.inventory
+  end
+
 end
