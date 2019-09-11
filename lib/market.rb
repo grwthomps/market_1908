c88e344a25351315a955fa335fcde63313593e84
Graham Thompson <grwthomps@gmail.com>
2019-09-11 14:14:34 -0600

    Create sorted_item_list and total_inventory methods in market class

--- lib/market.rb
+++ lib/market.rb
@@ -23,4 +23,30 @@ class Market
     end
   end
 
+  def sorted_item_list
+    all_market_items = []
+    @vendors.each do |vendor|
+      vendor.all_items_in_stock.each do |item|
+        all_market_items << item
+      end
+    end
+    all_market_items.uniq.sort
+  end
+
+  def total_inventory
+    inventory = Hash.new(0)
+
+    sorted_item_list.each do |item|
+      inventory[item] += 0
+    end
+
+    inventory.each do |item, amt|
+      @vendors.each do |vendor|
+        inventory[item] += vendor.check_stock(item)
+      end
+    end
+
+    inventory
+  end
+
 end
