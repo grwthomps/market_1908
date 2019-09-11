ad918bc9fae447075f2eedbc8097afcce6557999
Graham Thompson <grwthomps@gmail.com>
2019-09-11 14:58:42 -0600

    Create sell method in market class

--- lib/market.rb
+++ lib/market.rb
@@ -45,8 +45,31 @@ class Market
         inventory[item] += vendor.check_stock(item)
       end
     end
-
     inventory
+  end
+
+  def sell(sell_item, sell_amt)
+    check_inv = total_inventory.select do |item, amt|
+      item == sell_item
+    end
+
+    return false if check_inv == {}
+    return false if check_inv.values[0] < sell_amt
+
+    vendors_that_have_item = vendors_that_sell(sell_item)
+
+    sell_counter = 0
+    vendors_that_have_item.each do |vendor|
+      if sell_counter != sell_amt
+        if vendor.check_stock(sell_item) >= sell_amt
+          vendor.inventory[sell_item] -= sell_amt
+          sell_counter += sell_amt
+        else vendor.inventory[sell_item] -= vendor.check_stock(sell_item)
+        end
+      end
+    end
+
+    return true
   end
 
 end
